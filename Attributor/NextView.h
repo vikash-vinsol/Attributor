2d79e767adf3846d03bdca2c18cf9a90230f2455
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-22 19:06:41 +0530

    Add two View Files

---
+++ Attributor/NextView.h
@@ -0,0 +1,13 @@
+//
+//  NextView.h
+//  Attributor
+//
+//  Created by Vikash Soni on 22/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextView : UIView
+
+@end
